417d27ec57664dd2494a3bbf35564b4738d7422b
denvernash <35231540+denvernash@users.noreply.github.com>
2018-02-06 14:15:04 -0500

    adding twitter step4 file

---
+++ twitter-oauth-step4.py
@@ -0,0 +1,26 @@
+
+from requests_oauthlib import OAuth1Session
+import secrets
+import json
+
+client_key = secrets.client_key
+client_secret = secrets.client_secret
+
+resource_owner_key = secrets.access_token
+resource_owner_secret = secrets.access_token_secret
+
+protected_url = 'https://api.twitter.com/1.1/account/settings.json'
+
+oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret)
+
+protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {'q':'food'}
+r = oauth.get(protected_url, params=params)
+python_object = json.loads(r.text)
+for x in python_object["statuses"]:
+    print("Author: ", x["user"]["screen_name"])
+    print("Text: ", x["text"])
+    print("")
